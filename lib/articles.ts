6df7f9bc047b6653c3e95bcf8ad6e627b7d8b328
Aadesh Rajbhandari <cresthaadork124@gmail.com>
2024-10-22 18:22:05 +1000

    second comit

--- lib/articles.ts
+++ lib/articles.ts
@@ -6,6 +6,7 @@ import { remark } from "remark"
 import html from "remark-html"
 
 import type {ArticleItem } from "@/types"
+import { title } from "process"
 
 const articlesDirectory = path.join(process.cwd(), "articles")
 
@@ -56,6 +57,23 @@ export const getCategorisedArticles = (): Record<string, ArticleItem[]> => {
 }
 
 export const getArticleData = async (id: string) => {
+    const fullPath = path.join(articlesDirectory, `${id}.md`)
 
+    const fileContents = fs.readFileSync(fullPath,"utf-8")
+
+    const matterResult = matter(fileContents)
+
+    const processedContent= await remark()
+    .use(html)
+    .process(matterResult.content)
     
+    const contentHtml = processedContent.toString()
+
+    return{
+        id,
+        contentHtml,
+        title: matterResult.data.title,
+        category: matterResult.data.category,
+        date: moment(matterResult.data.date, "DD-MM-YYYY").format("MMMM Do YYYY"),
+    }
 }
